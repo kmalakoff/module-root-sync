8f29a325d28e88b1c8f5bb7e7d7661c39160f794
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-28 16:26:28 -0700

    Allow custom filename in RootOptions

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -5,16 +5,31 @@ import path from 'path';
 import url from 'url';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
+const DATA = path.join(__dirname, '..', 'data');
 
 describe('module-root-sync', () => {
-  it('finds root', () => {
+  it('finds root package.json', () => {
     const root = moduleRoot(__dirname);
     assert.equal(root, path.dirname(path.dirname(__dirname)));
   });
 
-  it('does not find root', () => {
+  it('finds data tsconfig.json', () => {
+    const root = moduleRoot(DATA, { name: 'tsconfig.json' });
+    assert.equal(root, DATA);
+  });
+
+  it('does not find root - keyExists', () => {
     try {
       moduleRoot(__dirname, { keyExists: 'junkityjunk' });
+      assert.ok(false);
+    } catch (err) {
+      assert.ok(err.message.indexOf('Root not found') === 0);
+    }
+  });
+
+  it('does not find root - name', () => {
+    try {
+      moduleRoot(DATA, { name: 'who-is.json' });
       assert.ok(false);
     } catch (err) {
       assert.ok(err.message.indexOf('Root not found') === 0);
