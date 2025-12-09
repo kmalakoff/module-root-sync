26789333358e2bfc4c2390a68d0fcdc740fda3dd
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-09 09:22:41 -0800

    Add support for file:// protocol and URL

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,6 @@ export interface RootOptions {
   name?: string;
   keyExists?: string;
 }
+
+/** A filesystem path string or file:// URL (string or URL object) */
+export type PathLike = string | URL;
